4e8c5e9bcf71962bff5a6b773b0289295ca93a24
Steve Heim <heim@is.mpg.de>
2019-02-19 15:26:09 +0100

    Update the packaging of the dg_tools package

---
+++ include/dg_tools/control/control_pd.hh
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2010,
+ * François Bleibel,
+ * Olivier Stasse,
+ *
+ * CNRS/AIST
+ *
+ * This file is part of sot-core.
+ * sot-core is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * sot-core is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.  You should
+ * have received a copy of the GNU Lesser General Public License along
+ * with sot-core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __SOT_Control_PD_HH__
+#define __SOT_Control_PD_HH__
+
+/* --------------------------------------------------------------------- */
+/* --- INCLUDE --------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+/* Matrix */
+#include <dynamic-graph/linear-algebra.h>
+namespace dg = dynamicgraph;
+
+/* SOT */
+#include <dynamic-graph/signal-time-dependent.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/entity.h>
+
+
+/* --------------------------------------------------------------------- */
+/* --- API ------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+#if defined (WIN32) 
+#  if defined (control_pd_EXPORTS)
+#    define ControlPD_EXPORT __declspec(dllexport)
+#  else  
+#    define ControlPD_EXPORT  __declspec(dllimport)
+#  endif 
+#else
+#  define ControlPD_EXPORT
+#endif
+
+namespace dynamicgraph {
+  namespace sot {
+
+  /* --------------------------------------------------------------------- */
+  /* --- CLASS ----------------------------------------------------------- */
+  /* --------------------------------------------------------------------- */
+
+  class ControlPD_EXPORT ControlPD
+    : public Entity
+    {
+
+    public: /* --- CONSTRUCTOR ---- */
+
+      ControlPD( const std::string & name );
+
+    public: /* --- INIT --- */
+
+      void init( const double& step);
+
+    public: /* --- CONSTANTS --- */
+
+      /* Default values. */
+      static const double TIME_STEP_DEFAULT;   // = 0.001
+
+    public: /* --- ENTITY INHERITANCE --- */
+      static const std::string CLASS_NAME;
+      virtual void display( std::ostream& os ) const; 
+      virtual const std::string& getClassName( void ) const {return CLASS_NAME;}
+
+
+    protected: 
+  
+      /* Parameters of the torque-control function: 
+       * tau = kp * (qd-q) + kd* (dqd-dq) */
+      double TimeStep;
+      double _dimension;
+
+    public:  /* --- SIGNALS --- */
+
+      SignalPtr<dg::Vector,int> KpSIN;
+      SignalPtr<dg::Vector,int> KdSIN;
+      SignalPtr<dg::Vector,int> positionSIN;
+      SignalPtr<dg::Vector,int> desiredpositionSIN;
+      SignalPtr<dg::Vector,int> velocitySIN;
+      SignalPtr<dg::Vector,int> desiredvelocitySIN;
+      SignalTimeDependent<dg::Vector,int> controlSOUT;
+      // SignalTimeDependent<dg::Vector,int> positionErrorSOUT;
+      // SignalTimeDependent<dg::Vector,int> velocityErrorSOUT;
+
+    protected:
+
+      double& setsize(int dimension);
+      dg::Vector& computeControl( dg::Vector& tau,int t );
+      dg::Vector positionError;
+      dg::Vector velocityError;
+      // dg::Vector& getPositionError( dg::Vector& positionError,int t );
+      // dg::Vector& getVelocityError( dg::Vector& velocityError,int t );
+
+    };
+
+
+
+} // namespace sot
+} // namespace dynamicgraph
+
+
+
+#endif // #ifndef __SOT_Control_PD_HH__
---
+++ src/control/control_pd.cpp
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2010,
+ * François Bleibel,
+ * Olivier Stasse,
+ *
+ * CNRS/AIST
+ *
+ * This file is part of sot-core.
+ * sot-core is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * sot-core is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.  You should
+ * have received a copy of the GNU Lesser General Public License along
+ * with sot-core.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* SOT */
+#include <dg_tools/control/control_pd.hh>
+
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+#include <sot/core/debug.hh>
+#include <dynamic-graph/factory.h>
+
+using namespace dynamicgraph::sot;
+using namespace dynamicgraph;
+
+DYNAMICGRAPH_FACTORY_ENTITY_PLUGIN(ControlPD,"ControlPD");
+
+const double ControlPD::
+TIME_STEP_DEFAULT = .001;
+
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+
+#define __SOT_ControlPD_INIT \
+
+ControlPD::
+ControlPD( const std::string & name )
+ :Entity(name)
+ ,TimeStep(0)
+ ,KpSIN(NULL,"ControlPD("+name+")::input(vector)::Kp")
+ ,KdSIN(NULL,"ControlPD("+name+")::input(vector)::Kd")
+ ,positionSIN(NULL,"ControlPD("+name+")::input(vector)::position")
+ ,desiredpositionSIN(NULL,"ControlPD("+name+")::input(vector)::desiredposition")
+ ,velocitySIN(NULL,"ControlPD("+name+")::input(vector)::velocity")
+ ,desiredvelocitySIN(NULL,"ControlPD("+name+")::input(vector)::desiredvelocity")
+ ,controlSOUT( boost::bind(&ControlPD::computeControl,this,_1,_2),
+         KpSIN << KdSIN << positionSIN << desiredpositionSIN
+         << velocitySIN << desiredvelocitySIN,
+        "ControlPD("+name+")::output(vector)::control" )
+{
+  init(TimeStep);
+  Entity::signalRegistration( KpSIN << KdSIN << positionSIN << 
+    desiredpositionSIN << velocitySIN << desiredvelocitySIN << controlSOUT );
+}
+
+void ControlPD::
+init(const double& Stept)
+{
+  TimeStep = Stept;
+
+  return;
+}
+
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+void ControlPD::
+display( std::ostream& os ) const
+{
+  os << "ControlPD "<<getName();
+  try{
+    os <<"control = "<<controlSOUT; 
+  }
+  catch (ExceptionSignal e) {}
+  os <<" ("<<TimeStep<<") ";
+}
+
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+double& ControlPD::setsize(int dimension)
+
+{
+  _dimension = dimension;
+        return _dimension;
+}
+
+dynamicgraph::Vector& ControlPD::
+computeControl( dynamicgraph::Vector &tau, int t ) 
+{
+  sotDEBUGIN(15);     
+  const dynamicgraph::Vector& Kp = KpSIN(t);
+  const dynamicgraph::Vector& Kd = KdSIN(t);      
+  const dynamicgraph::Vector& position = positionSIN(t);
+  const dynamicgraph::Vector& desiredposition = desiredpositionSIN(t);
+  const dynamicgraph::Vector& velocity = velocitySIN(t);      
+  const dynamicgraph::Vector& desiredvelocity = desiredvelocitySIN(t);
+      
+  dynamicgraph::Vector::Index size = Kp.size();   
+  tau.resize(size);
+  positionError.resize(size);
+  velocityError.resize(size);
+
+  positionError.array() = desiredposition.array()-position.array();  
+  velocityError.array() = desiredvelocity.array()-velocity.array();
+
+  tau.array() = positionError.array()*Kp.array() 
+              + velocityError.array()*Kd.array();
+  
+  sotDEBUGOUT(15);
+ // std::cout << " tau " << tau << std::endl;
+ // std::cout << "velocity " << velocity << std::endl;
+  return tau;
+
+}
+
+
+
