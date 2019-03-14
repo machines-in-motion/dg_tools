36033239714be13cec1f8366e6c23e25c4364e87
Julian Viereck <jviereck@tuebingen.mpg.de>
2019-03-14 14:32:11 -0400

    Adding MemoryReplay and PreviousValue entity

---
+++ include/dg_tools/data/memory_replay.hpp
@@ -0,0 +1,71 @@
+/**
+ * Copyright 2019 Max Planck Society. All rights reserved.
+ * Julian Viereck
+ */
+
+#pragma once
+
+/* --------------------------------------------------------------------- */
+/* --- INCLUDE --------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+/* Matrix */
+#include <dynamic-graph/linear-algebra.h>
+namespace dg = dynamicgraph;
+
+/* SOT */
+#include <dynamic-graph/signal-time-dependent.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/entity.h>
+
+
+/* --------------------------------------------------------------------- */
+/* --- API ------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+
+#if defined (WIN32)
+#  if defined (MemoryReplay_EXPORTS)
+#    define MemoryReplay_EXPORTS __declspec(dllexport)
+#  else
+#    define MemoryReplay_EXPORTS  __declspec(dllimport)
+#  endif
+#else
+#  define MemoryReplay_EXPORTS
+#endif
+
+namespace dg_tools {
+
+  /* --------------------------------------------------------------------- */
+  /* --- CLASS ----------------------------------------------------------- */
+  /* --------------------------------------------------------------------- */
+
+  /**
+   * @brief Provided with a matrix, the entity returns the row indexed by
+   * the current time index at every timestep.
+   *
+   * Example: Store a desired trajectory in memory and get the desired position
+   * at every timestep.
+   */
+  class MemoryReplay_EXPORTS MemoryReplay: public dg::Entity
+  {
+    public:
+
+      MemoryReplay( const std::string & name );
+
+      void init(const dg::Matrix& data);
+      void rewind();
+
+      static const std::string CLASS_NAME;
+      virtual const std::string& getClassName( void ) const {return CLASS_NAME;}
+
+      dg::SignalTimeDependent<dg::Vector,int> internal_signal_refresher_;
+      dg::SignalTimeDependent<dg::Vector,int> sout;
+
+      dg::Vector& getValue(dg::Vector& sout, int time);
+
+    private:
+      bool is_started_;
+      int start_time_;
+      dg::Matrix data_;
+  };
+} // namespace dg_tools
