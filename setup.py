53c1dcb9d9eabe111b4cffbb1b78b65d914ca558
Avadesh Meduri <ameduri@denali.is.localnet>
2019-03-22 15:37:42 +0100

    for pull

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+package_info = generate_distutils_setup()
+package_info['packages'] = ['']
+package_info['package_dir'] = {'': 'python'}
+package_info['install_requires'] = []
+setup(**package_info)
