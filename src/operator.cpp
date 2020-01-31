d5863fb79ea72281f70f0fe50d2fb5d1ca452f87
Julian Viereck <jviereck@tuebingen.mpg.de>
2020-01-31 11:07:43 +0100

    PoseQuaternionToPoseRPY: Fix conversion to rpy angles

--- src/operator.cpp
+++ src/operator.cpp
@@ -39,12 +39,16 @@ PoseQuaternionToPoseRPY::PoseQuaternionToPoseRPY( const std::string & name )
 dg::Vector& PoseQuaternionToPoseRPY::data_out_callback(dg::Vector& out, int time)
 {
     const dg::Vector& input = data_inputSIN(time);
-    dynamicgraph::sot::VectorQuaternion quat;
-    quat.coeffs() = input.tail<4>();
+    dynamicgraph::sot::VectorQuaternion quat(input(6), input(3), input(4), input(5));
+
+    std::cout << "Input:" << input << std::endl;
+
+    std::cout << "quatcof:" << quat.coeffs() << std::endl;
+    std::cout << "rotmat:" << quat.toRotationMatrix() << std::endl;
 
     out.resize(6);
     out.head<3>() = input.head<3>();
-    out.tail<3>() = quat.toRotationMatrix().eulerAngles(0, 1, 2);
+    out.tail<3>() = quat.toRotationMatrix().eulerAngles(2, 1, 0).reverse();
     return out;
 }
 
--- tests/test_operator.py
+++ tests/test_operator.py
@@ -3,6 +3,13 @@ import numpy as np
 
 from dynamic_graph_manager.dg_tools import PoseQuaternionToPoseRPY
 
+import pinocchio as pin
+
+def posquat2posrpy(pos):
+    pos = np.matrix(pos).T
+    se3 = pin.utils.XYZQUATToSe3(pos)
+    return np.array(np.hstack([pos.T[:, :3], pin.rpy.matrixToRpy(se3.rotation).T])).reshape(-1)
+
 class TestPoseQuaternionToPoseRPY(unittest.TestCase):
     def test_basic(self):
         op = PoseQuaternionToPoseRPY('')
@@ -10,3 +17,13 @@ class TestPoseQuaternionToPoseRPY(unittest.TestCase):
 
         op.sout.recompute(10)
         np.testing.assert_array_equal(op.sout.value, [0, 1, 2, 0, 0, 0])
+
+    def test_rotated(self):
+        q0 = (0.34245364134360234, 0.3289527217213123, 0.1978762653232481,
+            0.002508853212199517, 0.016383906408659818, 0.20479396648554946, 0.9786647559596021)
+
+        pq2pr = PoseQuaternionToPoseRPY('')
+        pq2pr.sin.value = q0
+        pq2pr.sout.recompute(1)
+
+        np.testing.assert_allclose(posquat2posrpy(q0), pq2pr.sout.value, rtol=1e-4)
